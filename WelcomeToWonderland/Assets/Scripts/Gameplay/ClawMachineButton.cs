f0d202e7114c08ce6655fa686c89266cf95d6ced
Joel Gabriel <joelybahh@gmail.com>
2017-10-13 16:54:14 +1100

    FUCK MATHS but I did this...
    #138 4hrs of claw machine angular maths and state machine flow, my brain is now moosh, hell yeah

---
+++ WelcomeToWonderland/Assets/Scripts/Gameplay/ClawMachineButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NewtonVR;
+using UnityEngine.Events;
+using WW.Physics;
+
+public class ClawMachineButton : MonoBehaviour {
+    [Header("Claw Machine Lever Control Reference")]
+    [SerializeField] private ClawMachineLever m_clawMachineRef;
+
+    [Header ("Button Events")]
+    [SerializeField] private UnityEvent m_onPressed;
+
+    [Header ("Audio Settings")]
+    [SerializeField] private AudioClip m_clip;
+    [SerializeField] private AudioSource m_source;
+
+    private NVRButton m_button;
+
+    void Start () {
+        m_button = GetComponent<NVRButton> ();
+    }
+
+    void Update () {
+        if (m_button.ButtonWasPushed) {
+            m_onPressed.Invoke ();
+            m_source.PlayOneShot (m_clip);
+            m_clawMachineRef.CurMachineState = eMachineState.GRABBING;
+        }
+    }
+}
---
+++ WelcomeToWonderland/Assets/Scripts/Gameplay/ClawMachineClaw.cs
@@ -0,0 +1,26 @@
+﻿using WW.Physics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ClawMachineClaw : MonoBehaviour {
+
+    [SerializeField] private ClawMachineLever m_clawMachineLever;
+
+    [SerializeField] private int m_randResult;
+
+    [Range(0,100)]
+    [SerializeField] private int m_percentChanceOfGrab;
+
+    private void OnTriggerEnter(Collider col) {
+        m_clawMachineLever.HasItem = true;
+
+        m_randResult = Random.Range (0, 100);
+
+        if (m_randResult < m_percentChanceOfGrab) {
+            if (m_clawMachineLever.GrabbedItem == null)
+                m_clawMachineLever.GrabbedItem = col.GetComponent<Rigidbody> ();
+        }
+    }
+}
