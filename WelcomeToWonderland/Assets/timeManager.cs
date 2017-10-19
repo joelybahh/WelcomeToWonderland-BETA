2d8cf39eba3b75b515db6bf89c7b743845103907
MatthewJ97 <MatthewJ97@users.noreply.github.com>
2017-10-19 14:10:15 +1100

    added highlight (hope it works), added correct formating for the timer, fixed load scene

--- WelcomeToWonderland/Assets/timeManager.cs
+++ WelcomeToWonderland/Assets/timeManager.cs
@@ -7,9 +7,9 @@ using UnityEngine.Events;
 namespace WW.Managers
 {
 
-    public class timeManager : MonoBehaviour
+    public class TimeManager : MonoBehaviour
     {
-        public static timeManager Instance;
+        public static TimeManager Instance;
         bool PlayerWin = false;
         public float minutes = 1, seconds = 0;
         public Text display;
@@ -28,30 +28,31 @@ namespace WW.Managers
         // Update is called once per frame
         void Update()
         {
-
-
-
+            //if loser
             if ( seconds <= 0 && minutes <= 0 && !PlayerWin ) {
                 s.LoadScene(5, 0);
                 ExplosionParticle.Play();
             }
+            //if winner
             else if ( seconds <= 0 && minutes <= 0 && PlayerWin ) {
                 s.LoadScene(18, 0);
                 ConfettiParticle.Play();
             }
             else seconds -= Time.deltaTime;
+            //countdown a minute
             if (seconds < 0 && minutes > 0 )
             {
                 seconds = 60;
                 if (minutes > 0) minutes -= 1;
 
             }
-
+            //call final countdown
             if (minutes == 0 && (int)seconds == 15)
             {
                 AudioManager.Instance.PlayVoiceLine(36);
             }
-            display.text = (minutes + ":" + (int)seconds);
+            if(seconds > 10)display.text = (minutes + ":" + (int)seconds);
+            else display.text = (minutes + ":0" + (int)seconds);
 
             if (minutes == 0 && (int)seconds <= 15)
             {
@@ -66,7 +67,7 @@ namespace WW.Managers
         /// Sets minutes
         /// </summary>
         /// <param name="a">Minutes</param>
-        public void setMinutes(int a)
+        public void SetMinutes(int a)
         {
             minutes = a;
 
