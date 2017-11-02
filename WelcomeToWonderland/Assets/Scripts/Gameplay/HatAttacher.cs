9170d064459c81a64675373d4c3bee587bf70090
Joel Gabriel <joelybahh@gmail.com>
2017-11-02 16:46:49 +1100

    #157  Fixed lag in hallway, 45m #158  Trailer scene setup and ready to go, 10m           Programmed Temp Trailer Beginning, 15m           Programmed hat wearing feature, 15m           Placed temp props in bar, 15m           Recorded trailer footage with dion 30m

---
+++ WelcomeToWonderland/Assets/Scripts/Gameplay/HatAttacher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatAttacher : MonoBehaviour {
+
+    [SerializeField] private Rigidbody m_rb;
+
+	void OnTriggerEnter(Collider col) {
+        if(col.tag == "HatAttach") {
+            transform.parent = col.transform;
+            transform.position = col.transform.position;
+            transform.rotation = Quaternion.identity;
+            m_rb.isKinematic = true;
+        }
+    }
+
+    void OnTriggerExit ( Collider col ) {
+        if (col.tag == "HatAttach") {
+            transform.parent = null;
+            m_rb.isKinematic = false;
+        }
+    }
+}
