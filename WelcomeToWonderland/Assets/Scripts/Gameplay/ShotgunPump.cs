24d182bddb3179bf17f0d112d5aea60bcf789ffb
MatthewJ97 <MatthewJ97@users.noreply.github.com>
2017-11-09 10:42:25 +1100

    #shotgun

---
+++ WelcomeToWonderland/Assets/Scripts/Gameplay/ShotgunPump.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotgunPump : NewtonVR.NVRSlider
+{
+    bool InPump;
+    bool OutPump;
+    public bool pumped = false;
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+
+        if (!pumped)
+        {
+            if (OutPump && InPump){ pumped = true; return;  }
+
+            if (InPump && CurrentValue < 0.1f) { OutPump = true; return; }
+            if (!InPump && CurrentValue > 0.9f){ InPump = true; return; }
+        }
+    }
+
+}
