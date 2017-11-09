090eda384d1ffe719a6dbab7b121573d0aa286d9
MatthewJ97 <matthewjones1256@gmail.com>
2017-11-09 13:55:50 +1100

    floor state and seat buttons

---
+++ WelcomeToWonderland/Assets/Scripts/Puzzle/SeatButtonPuzzle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace WW.Puzzles {
+    public class SeatButtonPuzzle : Puzzle {
+        List<NewtonVR.NVRButton> buttons;
+        
+
+
+        // Update is called once per frame
+        void Update( ) {
+            m_completed = true;
+            foreach ( var b in buttons ) {
+                if ( !b.ButtonIsPushed ) m_completed = false; 
+            }
+            if ( m_completed ) CompletePuzzle();
+
+        }
+    }
+}
