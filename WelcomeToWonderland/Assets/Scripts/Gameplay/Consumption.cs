6348e01a3941e3497124c40154b27d2fa92cbb07
MatthewJ97 <matthewjones1256@gmail.com>
2017-11-09 11:35:18 +1100

    food for thought

---
+++ WelcomeToWonderland/Assets/Scripts/Gameplay/Consumption.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Consumption : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other) {
+        if ( other.tag == "Consumable" ) {
+            Destroy(other.gameObject);
+        }
+    }
+}
