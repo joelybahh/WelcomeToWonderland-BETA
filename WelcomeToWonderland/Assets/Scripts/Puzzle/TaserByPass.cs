f6b38a35d8151ecca54d16f59765d31b9eb92575
MatthewJ97 <matthewjones1256@gmail.com>
2017-11-09 11:11:53 +1100

    Taserbypass script

---
+++ WelcomeToWonderland/Assets/Scripts/Puzzle/TaserByPass.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WW.Puzzles {
+    public class TaserByPass : MonoBehaviour {
+
+        public Puzzle puzzle;
+
+        private void OnCollisionEnter(Collision collision) {
+            if ( collision.gameObject.tag == "Lighter" ) {
+                puzzle.CompletePuzzle();
+            }
+        }
+    }
+}
