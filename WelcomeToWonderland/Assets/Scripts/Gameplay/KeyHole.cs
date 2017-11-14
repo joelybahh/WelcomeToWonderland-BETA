8c4d48bc0188e13bac8a0f62f09bff70580c27c4
MatthewJ97 <matthewjones1256@gmail.com>
2017-11-14 22:15:27 +1100

    keyhole should be working, did some scene changes cause double up stage was killing me inside ,, rebaked navmesh to acoomadte changes

--- WelcomeToWonderland/Assets/Scripts/Gameplay/KeyHole.cs
+++ WelcomeToWonderland/Assets/Scripts/Gameplay/KeyHole.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class KeyHole : MonoBehaviour {
-
+    public string indexName;
+    public bool turned = true;
 	// Use this for initialization
-	void Start () {
-		
+	void OnTriggerEnter (Collider other) {
+		if(other.gameObject.tag == "LevelKey" && other.gameObject.GetComponent<LevelKey>().Name == indexName ) {
+
+            other.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
+
+        }
 	}
 	
 	// Update is called once per frame
---
+++ WelcomeToWonderland/Assets/Scripts/Gameplay/KeyHoleTooth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyHoleTooth : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other) {
+        if ( other.gameObject.tag == "LevelKey"  ) {
+            GetComponent<KeyHole>().turned = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if ( other.gameObject.tag == "LevelKey" ) {
+            GetComponent<KeyHole>().turned = false;
+        }
+    }
+}
